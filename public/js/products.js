751d584ce720a1743e4d3bdc54b3c563b53c2c51
Luis <luisbattle@hotmail.com.ar>
2021-01-27 00:42:36 -0300

    se agrega consume api offers desde la home con axios Desafio 3

--- public/js/products.js
+++ public/js/products.js
@@ -1,13 +1,10 @@
 console.log("test");
-
+//get products -> latest
 axios
   .get("/api/products/latest")
   .then((products) => {
-    //console.log(products["data"]["data"]);
-
     if (products["data"]["data"].length > 0) {
       products["data"]["data"].forEach((product) => {
-        console.log(product);
         products = `<div class="col-12 col-sm-6 col-lg-3" >
     <section class="product-box">
           <a href="/products/detail/${product.id}">
@@ -32,11 +29,51 @@ axios
       });
     } else {
       productTag = document.getElementById("latest-products");
-      productTag.innerHTML = `<div class="col-12" id="no-products" class="no-products-not-show">
-      <h2 class="noproducts" class="no-products-not-show">Aun no se encontraron productos</h2></div>`;
+      productTag.innerHTML = `<div class="col-12" id="no-products">
+      <h2 class="noproducts">Aun no se encontraron productos</h2></div>`;
     }
   })
   .catch((err) => {
     console.log(err);
-    //show error on page
+  });
+
+//get products -> offers
+axios
+  .get("/api/products/offers")
+  .then((offers) => {
+    //console.log(products["data"]["data"]);
+
+    if (offers["data"]["data"].length > 0) {
+      offers["data"]["data"].forEach((offers) => {
+        console.log(offers);
+        offers = `<div class="col-12 col-sm-6 col-lg-3">
+            <section class="product-box">
+              <a href="/products/detail/${offers.id} ">
+                <figure class="product-box_image">
+                  <img src="/images/products/${offers.image}" alt="${
+          offers.name
+        }">
+                </figure>
+                <article class="product-box_data">
+                  <h2>$ ${
+                    offers.price - offers.price * (offers.discount / 100)
+                  }</h2>
+                  <span> ${offers.discount} % OFF</span>
+                  <p>${offers.name}</p>
+                  <i class="fas fa-truck"></i>
+                </article>
+              </a>
+            </section>
+          </div>`;
+        productTag = document.getElementById("offers-products");
+        productTag.innerHTML += offers;
+      });
+    } else {
+      productTag = document.getElementById("offers-products");
+      productTag.innerHTML = `<div class="col-12" id="no-products">
+      <h2 class="noproducts">Aun no se encontraron productos</h2></div>`;
+    }
+  })
+  .catch((err) => {
+    console.log(err);
   });
