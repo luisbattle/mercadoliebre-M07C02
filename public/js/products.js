28f67632711d9a9d1a1af7e36c02b87823a779ed
Luis <luisbattle@hotmail.com.ar>
2021-01-27 00:16:17 -0300

    se agrega api y vista de productos ultimos creados

--- public/js/products.js
+++ public/js/products.js
@@ -4,30 +4,37 @@ axios
   .get("/api/products/latest")
   .then((products) => {
     //console.log(products["data"]["data"]);
-    products["data"]["data"].forEach((product) => {
-      console.log(product);
-      products = `<div class="col-12 col-sm-6 col-lg-3" >
-      <section class="product-box">
-            <a href="/products/detail/${product.id}">
-                <figure class="product-box_image">
-                    <img src="/images/products/${
-                      product.image
-                    }" alt="{product.name}">
-                </figure>
-                <article class="product-box_data">
-                    <h2>$ ${
-                      product.price - (product.price * product.discount) / 100
-                    }
-                    </h2>
-                    <p>${product.name}</p>
-                    <i class="fas fa-truck"></i>
-                </article>
-            </a>
-        </section>
-        </div>`;
+
+    if (products["data"]["data"].length > 0) {
+      products["data"]["data"].forEach((product) => {
+        console.log(product);
+        products = `<div class="col-12 col-sm-6 col-lg-3" >
+    <section class="product-box">
+          <a href="/products/detail/${product.id}">
+              <figure class="product-box_image">
+                  <img src="/images/products/${
+                    product.image
+                  }" alt="{product.name}">
+              </figure>
+              <article class="product-box_data">
+                  <h2>$ ${
+                    product.price - (product.price * product.discount) / 100
+                  }
+                  </h2>
+                  <p>${product.name}</p>
+                  <i class="fas fa-truck"></i>
+              </article>
+          </a>
+      </section>
+      </div>`;
+        productTag = document.getElementById("latest-products");
+        productTag.innerHTML += products;
+      });
+    } else {
       productTag = document.getElementById("latest-products");
-      productTag.innerHTML += products;
-    });
+      productTag.innerHTML = `<div class="col-12" id="no-products" class="no-products-not-show">
+      <h2 class="noproducts" class="no-products-not-show">Aun no se encontraron productos</h2></div>`;
+    }
   })
   .catch((err) => {
     console.log(err);
